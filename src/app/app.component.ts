3495a339ebb474a53ff18d6aa28a588d2ac0c91f
Ruben GUINEZ GUTIERREZ <ruben.guinez@outlook.com>
2018-06-11 01:48:17 -0400

    Simple sample in angular about how to call an a service like API REST

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Http, Response} from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Indicadores económicos en Chile';
+    dateInspect = '';
+    valueUf = '';
+
+     constructor(private http: Http) {}
+
+     searchIndicator() {
+       this.http.get('https://mindicador.cl/api/uf/' + this.dateInspect)
+       .subscribe(
+           (res: Response) => {
+             const valueIndicator = res.json();
+             console.log(valueIndicator);
+             //this.valueUf =  valueIndicator.serie.valor; // res['serie'][0]['valor'];
+             this.valueUf = valueIndicator['serie'][0]['valor'];
+             //for(let miS of valueIndicator['serie']) {
+             //  console.log(miS);
+              // console.log(miS['valor']);
+            // }
+           }, err => {
+             console.log('UPS!');
+             console.log(err);
+           }, () => {
+             console.log('¡Servicio Finalizado!');
+             // https://angular.io/guide/quickstart
+             // https://materializecss.com/
+           }
+       );
+     }
+
+
+}
